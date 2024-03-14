85637b2ce6af4f4ba4d134696563c2b8717b2673
Elmer Camacho <renehiguera15@gmail.com>
2024-03-14 15:18:19 -0700

    avance swiper

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ front/src/app/carrusel/carrusel.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-carrusel',
+  templateUrl: './carrusel.component.html',
+  styleUrls: ['./carrusel.component.css']
+})
+export class CarruselComponent {
+  slider_images = [
+    "../../assets/img/tenis_1.jpeg",
+    "../../assets/img/tenis_2.jpeg"
+    
+  ]
+
+
+}
